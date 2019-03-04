9036fdded10197b9ccdeb4b58e94daa33cf3da96
ulrik magnusson <ulri1122@gmail.com>
2019-03-04 10:43:10 +0100

    fixed departments

--- index2.js
+++ index2.js
@@ -2,20 +2,23 @@
 
 $(document).ready(function () {
         
-    
+        
         $('#dep').click(function(){
-        var query = "SELECT * FROM departments order by dept_no"
+                var query = "SELECT * FROM departments order by dept_no"
                 $.getJSON("/departments?select="+ encodeURIComponent(query)  , function(data){
-                        
+                        table = "";
                         $.each(data, function(key, value){
                                 //alert(key + "----" + value)
                                 //$.each(value, function(dep_num)
-                                value["dept_no"]
-                                value["dept_name"]
+                                table += "<tr>"
+                                table += "<td>" + value["dept_no"]+ "</td>" + "<td>" + value["dept_name"]+ "</td>"
                                 
+                                table += "<tr>"
                         })
-
-                        console.log(data)
+                        //$('#deptList').html("<td>" + value["dept_no"]+ "</td>")   
+                        $('#deptList').html(table)
+                        
+                        console.log(table)
 
 
 
