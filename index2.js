52b98e138458d8bc25895649d66eabdabd9097d8
ulrik magnusson <ulri1122@gmail.com>
2019-03-05 10:07:32 +0100

    cleanup

--- index2.js
+++ index2.js
@@ -1,150 +1,169 @@
 
 
 $(document).ready(function () {
-        
-        
-        
-        $('#dep').click(function(){
+
+        debug = false
+
+        $('#dep').click(function () {
                 var query = "SELECT * FROM departments order by dept_no"
-                $.getJSON("/departments?select="+ encodeURIComponent(query)  , function(data){
+                $.getJSON("/departments?select=" + encodeURIComponent(query), function (data) {
                         table = ""
-                        $.each(data, function(key, value){
+                        $.each(data, function (key, value) {
 
                                 table += "<tr>"
-                                table += "<td><button class = 'dept_button' id ='"+value["dept_no"] +"'>" + value["dept_name"]+ "</button></td>"
-                                
+                                table += "<td><button class = 'dept_button' id ='" + value["dept_no"] + "'>" + value["dept_name"] + "</button></td>"
+
                                 table += "<tr>"
                         })
-                        
-                        $('#deptList').html(table)
-                        
-                        console.log(table)
 
-                        $('.dept_button').click ( function( ){
-                                
+                        $('#deptList').html(table)
+                        if (debug == true) {
+                                console.log(table)
+
+                        }
+
+                        $('.dept_button').click(function () {
+
                                 i = 20;
-                                
+
                                 tablename = $(this).attr("id");
-                                var query ="SELECT employees.*, dept_emp.dept_no FROM employees INNER JOIN dept_emp ON dept_emp.emp_no = employees.emp_no WHERE dept_no ='" + tablename+ "' LIMIT 0,"+i+";"
-                                $.getJSON("/employees?select="+ encodeURIComponent(query)  , function(data){
+                                var query = "SELECT employees.*, dept_emp.dept_no FROM employees INNER JOIN dept_emp ON dept_emp.emp_no = employees.emp_no WHERE dept_no ='" + tablename + "' LIMIT 0," + i + ";"
+                                $.getJSON("/employees?select=" + encodeURIComponent(query), function (data) {
                                         table = "<table>";
-                                        
-                                        $.each (data, function(key,value){
-                                                
-                                                table += "<tr><td>"+ value['first_name']+ "</td><td>" + value ['last_name'] 
-                                                + "</td><td>" + value['gender']+ "</td><td>" + value['birth_date'] + "</td><td>" + value['hire_date']+"</td></tr>" ;
-                                                
-                                                
+
+                                        $.each(data, function (key, value) {
+
+                                                table += "<tr><td>" + value['first_name'] + "</td><td>" + value['last_name']
+                                                        + "</td><td>" + value['gender'] + "</td><td>" + value['birth_date'] + "</td><td>" + value['hire_date'] + "</td></tr>";
+
+
                                         })
                                         table += "</table><button id = 'showMore'>show more</button>"
-                                        
+
                                         $('#employees').html(table)
-                                        
+
                                         $('#showMore').click(showmorebtn)
                                 })
                         })
                 });
         })
 
-        $('#insertbtn').click( function(event){
+        $('#insertbtn').click(function (event) {
                 event.preventDefault();
-                console.log("hello")
-                console.log($("#dept_input").val())
+                if (debug == true) {
+
+                        console.log($("#dept_input").val())
+                }
                 $.ajax({
                         global: false,
                         type: 'POST',
                         url: '/insert',
                         dataType: 'html',
                         data: {
-                            deptname: $("#dept_input").val()
-                            
+                                deptname: $("#dept_input").val()
+
                         },
                         success: function (result) {
-                            console.log(result);
+                                if (debug == true) {
+                                        console.log(result);
+
+                                }
                         },
                         error: function (request, status, error) {
-                            serviceError();
+                                serviceError();
                         }
-                    });
                 });
+        });
 
 
 
-                $('#searchthis').click(function(){
-                        i = 20
-                        
-                        event.preventDefault();
-                        var query = "SELECT * FROM employees WHERE first_name REGEXP '" + $("#searchInput").val() + "'  || last_name REGEXP '" + $("#searchInput").val() + "' LIMIT 0,"+i+";"
-                        $.getJSON("/search?select="+ encodeURIComponent(query), function(data){
+        $('#searchthis').click(function () {
+                i = 20
+
+                event.preventDefault();
+                var query = "SELECT * FROM employees WHERE first_name REGEXP '" + $("#searchInput").val() + "'  || last_name REGEXP '" + $("#searchInput").val() + "' LIMIT 0," + i + ";"
+                $.getJSON("/search?select=" + encodeURIComponent(query), function (data) {
+                        if (debug == true) {
                                 console.log(data)
+
+                        }
                         table = "<table>"
-                        $.each(data, function(key, value) {
-                                table += "<tr><td>"+ value['first_name']+ "</td><td>" + value ['last_name'] 
-                                + "</td><td>" + value['gender']+ "</td><td>" + value['birth_date'] + "</td><td>" + value['hire_date']+"</td></tr>"
+                        $.each(data, function (key, value) {
+                                table += "<tr><td>" + value['first_name'] + "</td><td>" + value['last_name']
+                                        + "</td><td>" + value['gender'] + "</td><td>" + value['birth_date'] + "</td><td>" + value['hire_date'] + "</td></tr>"
                         })
                         table += "</table><button id='showmoresearchbtn'>show more</button>"
                         $('#employees').html(table)
                         $('#showmoresearchbtn').click(showmoresearch)
-                        console.log(table)
+                        if (debug == true) {
+
+                                console.log(table)
+                        }
                 });
-        });     
-        
-        
+        });
 
-   /*     $('.navbar-collapse ul li a').click(function(){ 
-                $('.navbar-toggle:visible').click();
-        });*/
 
-                
+
+        /*     $('.navbar-collapse ul li a').click(function(){ 
+                     $('.navbar-toggle:visible').click();
+             });*/
+
+
 });
-var showmoresearch = function() {
+var showmoresearch = function () {
         i = i + 20
-        console.log(i)
-        var query ="SELECT * FROM employees WHERE first_name REGEXP '" + $("#searchInput").val() + "'  || last_name REGEXP '" + $("#searchInput").val() + "' LIMIT 0,"+i+";"
-        $.getJSON("/search?select="+ encodeURIComponent(query)  , function(data){
+        if (debug == true) {
+
+                console.log(i)
+        }
+        var query = "SELECT * FROM employees WHERE first_name REGEXP '" + $("#searchInput").val() + "'  || last_name REGEXP '" + $("#searchInput").val() + "' LIMIT 0," + i + ";"
+        $.getJSON("/search?select=" + encodeURIComponent(query), function (data) {
                 table = "<table>";
-                
-                $.each (data, function(key,value){
-                        
-                        table += "<tr><td>"+ value['first_name']+ "</td><td>" + value ['last_name'] +
-                         "</td><td>" + value['gender']+ "</td><td>" + value['birth_date'] + "</td><td>" + value['hire_date']+"</td></tr>" ;
-                        
-                        
+
+                $.each(data, function (key, value) {
+
+                        table += "<tr><td>" + value['first_name'] + "</td><td>" + value['last_name'] +
+                                "</td><td>" + value['gender'] + "</td><td>" + value['birth_date'] + "</td><td>" + value['hire_date'] + "</td></tr>";
+
+
                 })
                 table += "</table>";
 
                 table += "</table><button id = 'showmoresearchbtn'>show more</button>"
-                
+
                 $('#employees').html(table)
                 $('#showmoresearchbtn').click(showmoresearch)
-                
+
         });
 
 }
 
 
 
-var showmorebtn = function( ){
+var showmorebtn = function () {
         i = i + 20
-        console.log(i)
-        var query ="SELECT employees.*, dept_emp.dept_no FROM employees INNER JOIN dept_emp ON dept_emp.emp_no = employees.emp_no WHERE dept_no ='" + tablename + "' LIMIT 0,"+i+";"
-        $.getJSON("/employees?select="+ encodeURIComponent(query)  , function(data){
+        if (debug == true) {
+
+                console.log(i)
+        }
+        var query = "SELECT employees.*, dept_emp.dept_no FROM employees INNER JOIN dept_emp ON dept_emp.emp_no = employees.emp_no WHERE dept_no ='" + tablename + "' LIMIT 0," + i + ";"
+        $.getJSON("/employees?select=" + encodeURIComponent(query), function (data) {
                 table = "<table>";
-                
-                $.each (data, function(key,value){
-                        
-                        table += "<tr><td>"+ value['first_name']+ "</td><td>" + value ['last_name'] +
-                         "</td><td>" + value['gender']+ "</td><td>" + value['birth_date'] + "</td><td>" + value['hire_date']+"</td></tr>" ;
-                        
-                        
+
+                $.each(data, function (key, value) {
+
+                        table += "<tr><td>" + value['first_name'] + "</td><td>" + value['last_name'] +
+                                "</td><td>" + value['gender'] + "</td><td>" + value['birth_date'] + "</td><td>" + value['hire_date'] + "</td></tr>";
+
+
                 })
                 table += "</table>";
 
                 table += "</table><button id = 'showMore'>show more</button>"
-                
+
                 $('#employees').html(table)
                 $('#showMore').click(showmorebtn)
-                
+
         });
 
 }
